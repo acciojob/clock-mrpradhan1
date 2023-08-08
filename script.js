9f4185d5fc2fa8308fad0dfa3d02a3d04a44635c
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-08-08 19:43:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function updateTimer() {
+	const timerElement = document.getElementById('timer');
+	const currentTime= new Date();
+	const options={
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric' ,
+		hour: 'numeric' ,
+		minute: 'numeric' ,
+		second: 'numeric',
+		timeZoneName: 'short'
+	};
+	const locale = 'en-IN';
+	const formattedTime = currentTime.toLocaleDateString(locale, options);
+	timerElement.textContent =`Current Time = ${formattedTime}`;
+}
+updateTimer();
+setInterval(updateTimer, 1000);
